5039e8ed1e19a5d382db5345fd01fb1dab4e053d
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-06 00:29:55 +0200

    style: create style header

---
+++ src/presentation/components/Header/styles.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const HeaderContainer = styled.header`
+  margin: 0px 5rem;
+`;
+export const HeaderNav = styled.nav`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 5rem;
+  padding: 1rem 0px;
+`;
+export const HeaderContent = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const HeaderLogo = styled.div`
+ > img {
+  width: 19rem;
+ }
+`;
