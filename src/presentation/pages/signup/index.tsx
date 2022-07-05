c49ac6b31d124aa5df648d97055c896689b5f383
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-05 23:37:04 +0200

    feat: create signup page

--- src/presentation/pages/signup/index.tsx
+++ src/presentation/pages/signup/index.tsx
@@ -1,12 +1,37 @@
 import React, { FunctionComponent } from 'react';
 
-//import { Container } from './styles';
-//import styles from './SignUp.module.css';
+import { FormContainer, Form, FormContent } from '@/shared/styles/Form';
+import { InputForm } from '@/presentation/components/Input';
+import { Button } from '@/presentation/components/Button';
+import { Link } from 'react-router-dom';
 
 export const SignUp: FunctionComponent = () => {
   return (
-    <div>
-      <h2>SignUp</h2>
-    </div>
+    <FormContainer>
+      <Form>
+        <h2>SignUp MyUnsplash</h2>
+
+        <FormContent>
+          <InputForm name="name" label="Name" placeholder="Enter your Name">
+            <i className="ri-user-6-line"></i>
+          </InputForm>
+
+          <InputForm name="email" label="Email" placeholder="Enter your Email">
+            <i className="ri-mail-line"></i>
+          </InputForm>
+
+          <InputForm type="password" name="password" label="Password" placeholder="Enter your password">
+            <i className="ri-lock-password-line"></i>
+          </InputForm>
+
+          <Button text="SigUp">
+            <i className="ri-user-add-line"></i>
+          </Button>
+        </FormContent>
+        <p>
+          Do you already have an account? <Link to="/user/login">Login</Link>
+        </p>
+      </Form>
+    </FormContainer>
   );
 };
