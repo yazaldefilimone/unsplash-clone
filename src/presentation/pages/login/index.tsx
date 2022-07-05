8f89108a2cd502f9078dc42731f64884df4e3ba7
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-05 17:54:40 +0200

    feat: add input

--- src/presentation/pages/login/index.tsx
+++ src/presentation/pages/login/index.tsx
@@ -1,12 +1,19 @@
+import { Input } from '@/presentation/components/Input';
 import React, { FunctionComponent } from 'react';
 
-//import { Container } from './styles';
-//import styles from './Login.module.css';
+import { LoginContainer, LoginForm } from '@/shared/pages/login';
 
 export const Login: FunctionComponent = () => {
   return (
-    <div>
-      <h2>Login</h2>
-    </div>
+    <LoginContainer>
+      <LoginForm>
+        <Input
+          name="name"
+          label="Name"
+          placeholder="Enter your Name"
+          error="ouve algum erro inesperado"
+        />
+      </LoginForm>
+    </LoginContainer>
   );
 };
