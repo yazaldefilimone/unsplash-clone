1462f303949c1940d4ae90f0246a7554e8708ab1
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-06 09:37:45 +0200

    style: create style of component create image]

--- src/presentation/components/Modal/styles.ts
+++ src/presentation/components/Modal/styles.ts
@@ -9,5 +9,5 @@ export const ModalContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  background-color: rgba(0,0,0,.2);
+  background-color: rgba(0,0,0,.3);
 `;
