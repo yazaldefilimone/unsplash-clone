329d0fe7888134ef80d0a29c73daf67092546b63
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-06 02:04:49 +0200

    feat: create board component

---
+++ src/presentation/components/Board/index.tsx
@@ -0,0 +1,90 @@
+import React, { FunctionComponent } from 'react';
+
+import { BoardContainer } from './styles';
+import { Image } from '../Image';
+export const Board: FunctionComponent = () => {
+  const desc =
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque qui molestias cum, nihil natus omnis sequi tenetur dolores doloribus quia quo delectus maxime soluta nobis sunt officia illum possimus numquam.';
+  return (
+    <BoardContainer>
+      <div>
+        <Image
+          className="image-1"
+          src="https://images.unsplash.com/photo-1504194104404-433180773017?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+          description={desc}
+          userName={'Yazalde Filimone'}
+        />
+        <Image
+          className="image-1"
+          src="https://images.unsplash.com/photo-1504194104404-433180773017?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+          description={desc}
+          userName={'Yazalde Filimone'}
+        />
+        <Image
+          className=""
+          src="https://images.unsplash.com/photo-1504194104404-433180773017?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+          description={desc}
+          userName={'Yazalde Filimone'}
+        />
+        <Image
+          className="image-1"
+          src="https://images.unsplash.com/photo-1504194104404-433180773017?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+          description={desc}
+          userName={'Yazalde Filimone'}
+        />
+      </div>
+      <div>
+        <Image
+          className="image-1"
+          src="https://images.unsplash.com/photo-1504194104404-433180773017?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+          description={desc}
+          userName={'Yazalde Filimone'}
+        />
+        <Image
+          className="image-1"
+          src="https://images.unsplash.com/photo-1504194104404-433180773017?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+          description={desc}
+          userName={'Yazalde Filimone'}
+        />
+        <Image
+          className=""
+          src="https://images.unsplash.com/photo-1504194104404-433180773017?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+          description={desc}
+          userName={'Yazalde Filimone'}
+        />
+        <Image
+          className="image-1"
+          src="https://images.unsplash.com/photo-1504194104404-433180773017?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+          description={desc}
+          userName={'Yazalde Filimone'}
+        />
+      </div>
+      <div>
+        <Image
+          className="image-1"
+          src="https://images.unsplash.com/photo-1504194104404-433180773017?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+          description={desc}
+          userName={'Yazalde Filimone'}
+        />
+        <Image
+          className="image-1"
+          src="https://images.unsplash.com/photo-1504194104404-433180773017?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+          description={desc}
+          userName={'Yazalde Filimone'}
+        />
+        <Image
+          className=""
+          src="https://images.unsplash.com/photo-1504194104404-433180773017?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+          description={desc}
+          userName={'Yazalde Filimone'}
+        />
+        <Image
+          className="image-1"
+          src="https://images.unsplash.com/photo-1504194104404-433180773017?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+          description={desc}
+          userName={'Yazalde Filimone'}
+        />
+      </div>
+    </BoardContainer>
+  );
+};
