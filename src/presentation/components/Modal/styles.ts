8186a2974e0a8a075c9f4e789f50b5885be673e9
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-06 09:22:54 +0200

    style: create style modal

---
+++ src/presentation/components/Modal/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const ModalContainer = styled.div`
+  position: fixed;
+  top:0px;
+  left: 0px;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: rgba(0,0,0,.2);
+`;
