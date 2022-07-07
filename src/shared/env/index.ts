878ef10ba39481768eb88b64347f59908f3ee421
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-07 22:57:05 +0200

    fix: renome firebase and add unsplash api

--- src/shared/env/index.ts
+++ src/shared/env/index.ts
@@ -1,13 +1,8 @@
 export const env = {
-  firebase: {
-    key: process.env.FIREBASE_Key as string,
-    authDomain: process.env.FIREBASE_AuthDomain as string,
-    projectId: process.env.FIREBASE_ProjectId as string,
-    storageBucket: process.env.FIREBASE_StorageBucket as string,
-    messagingSenderId: process.env.FIREBASE_MessagingSenderId as string,
-    appId: process.env.FIREBASE_AppId as string,
-  },
   secret: {
     key: process.env.SECRET as string,
+  },
+  unsplash: {
+    api: process.env.UNSPLASH_API,
   },
 };
