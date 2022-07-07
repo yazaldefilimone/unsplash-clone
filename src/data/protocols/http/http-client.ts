72217eef27fea9efb3757132db353acbc5e17080
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-07 23:28:44 +0200

    fix: add string in method

--- src/data/protocols/http/http-client.ts
+++ src/data/protocols/http/http-client.ts
@@ -1,27 +1,26 @@
 export type HttpRequest = {
-  url: string
-  method: HttpMethod
-  body?: any
-  headers?: any
-}
+  url: string;
+  method: string;
+  body?: any;
+  headers?: any;
+};
 
 export interface HttpClient<R = any> {
-  request: (data: HttpRequest) => Promise<HttpResponse<R>>
+  request: (data: HttpRequest) => Promise<HttpResponse<R>>;
 }
-
-export type HttpMethod = 'post' | 'get' | 'put' | 'delete'
 
 export enum HttpStatusCode {
   ok = 200,
+  create = 201,
   noContent = 204,
   badRequest = 400,
   unauthorized = 401,
   forbidden = 403,
   notFound = 404,
-  serverError = 500
+  serverError = 500,
 }
 
 export type HttpResponse<T = any> = {
-  statusCode: HttpStatusCode
-  body?: T
-}
+  statusCode: HttpStatusCode;
+  body?: T;
+};
