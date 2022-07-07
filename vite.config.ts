5432fe0ab583c6b0eb6736a1988daf2851b7badd
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-08 00:52:53 +0200

    chore:  add vite-plugin-environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { ViteAliases } from 'vite-aliases';
+import EnvironmentPlugin from 'vite-plugin-environment';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    EnvironmentPlugin(['UNSPLASH_API']),
     ViteAliases({
       dir: 'src',
       prefix: '@',
