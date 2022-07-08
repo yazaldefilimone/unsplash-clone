df1642a6d0e5746f56e8dc550847c22e2cefc162
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-08 14:42:30 +0200

    feat: using create image usecase

--- src/domain/entities/image.ts
+++ src/domain/entities/image.ts
@@ -2,5 +2,4 @@ export type Image = {
   id?: string;
   label: string;
   url: string;
-  userId: string;
 };
--- src/presentation/components/CreateImage/index.tsx
+++ src/presentation/components/CreateImage/index.tsx
@@ -1,19 +1,60 @@
 import { Form, FormContent } from '@/shared/styles/Form';
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 import { Button } from '@/presentation/components/Button';
 import { InputForm } from '@/presentation/components/Input';
 
 import { CreateImageHeader } from './styles';
+import { ICreateImageUseCase } from '@/domain/usecases/image';
+import { useNavigate } from 'react-router-dom';
+import { IToastProps } from '@/presentation/components/Toast';
 type CreateImageProps = {
   SetActive: Function;
+  createImageUseCase: ICreateImageUseCase;
 };
 
-export const CreateImage: FunctionComponent<CreateImageProps> = ({ SetActive }) => {
-  const [title, SetTitle] = React.useState('');
+export const CreateImage: FunctionComponent<CreateImageProps> = ({ SetActive, createImageUseCase }) => {
+  const [label, SetLabel] = React.useState('');
   const [url, SetUrl] = React.useState('');
+  const [toast, SetToast] = useState(false);
+  const [loading, SetLoading] = useState(false);
+  const [errorOrSuccess, SetErrorOrSuccess] = useState<IToastProps>({} as IToastProps);
+  const navigate = useNavigate();
 
   function handlerClose() {
     SetActive((active: boolean) => !active);
+  }
+
+  async function handlerSubmit() {
+    try {
+      SetLoading(true);
+      const result = await createImageUseCase.perform({ url, label });
+      console.log(result);
+      SetLoading(false);
+      if (result.isLeft()) {
+        SetErrorOrSuccess({
+          message: result.value.message,
+          status: 'Error',
+        });
+
+        SetToast(true);
+        return 0;
+      }
+
+      SetErrorOrSuccess({
+        message: 'create Image successfully',
+        status: 'Success',
+      });
+      SetToast(true);
+      setTimeout(() => navigate('/'), 2000);
+    } catch (error: any) {
+      SetLoading(false);
+      SetErrorOrSuccess({
+        message: error.message,
+        status: 'Error',
+      });
+    } finally {
+      SetLoading(false);
+    }
   }
   return (
     <Form>
@@ -25,13 +66,13 @@ export const CreateImage: FunctionComponent<CreateImageProps> = ({ SetActive }) 
         </button>
       </CreateImageHeader>
       <FormContent>
-        <InputForm value={title} SetValue={SetTitle} name="title" label="Title">
+        <InputForm value={label} SetValue={SetLabel} name="label" label="Label">
           <i className="ri-price-tag-2-line"></i>
         </InputForm>
         <InputForm value={url} SetValue={SetUrl} name="url" label="Url">
           <i className="ri-link"></i>
         </InputForm>
-        <Button text="Create">
+        <Button text="Create" spinner={loading} SetActive={handlerSubmit}>
           <i className="ri-image-add-line"></i>
         </Button>
       </FormContent>
