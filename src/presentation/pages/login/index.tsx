8f5a68f7f00d780a5d0845c41f501d863d50acd4
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-08 11:32:15 +0200

    feat: create login page implemented usecase

--- src/presentation/pages/login/index.tsx
+++ src/presentation/pages/login/index.tsx
@@ -3,77 +3,101 @@ import React, { FunctionComponent, useState } from 'react';
 
 import { FormContainer, Form, FormContent } from '@/shared/styles/Form';
 import { Button } from '@/presentation/components/Button';
-import { Link } from 'react-router-dom';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
 import logo from '@/shared/assets/images/logo.svg';
 import { ILoginUserUseCase } from '@/domain/usecases/user';
-import { Toast } from '@/presentation/components/Toast';
+import { IToastProps, Toast } from '@/presentation/components/Toast';
 import { useToast } from '@/presentation/hooks/useToast';
+import { UnexpectedError } from '@/domain/errors';
 
 type LoginProps = {
   loginUserUseCase: ILoginUserUseCase;
 };
+
 export const Login: FunctionComponent<LoginProps> = ({ loginUserUseCase }) => {
-  const { SetToast, toast } = useToast();
-  // const [toast, SetToast] = useState(true);
+  const [toast, SetToast] = useState(false);
+  const [loading, SetLoading] = useState(false);
+  const [errorOrSuccess, SetErrorOrSuccess] = useState<IToastProps>({} as IToastProps);
   const [email, SetEmail] = React.useState('');
   const [password, SetPassword] = React.useState('');
+  const navigate = useNavigate();
 
   async function handlerSubmit() {
-    SetToast({
-      message: 'Invalid param [name]',
-      status: 'Error',
-      value: true,
-    });
+    try {
+      SetLoading(true);
+      const result = await loginUserUseCase.perform({ email, password });
+      console.log(result);
+      SetLoading(false);
+      if (result.isLeft()) {
+        SetErrorOrSuccess({
+          message: result.value.message,
+          status: 'Error',
+        });
 
-    // console.log(toast);
-    // try {
-    //   SetLoading(true);
-    //   const result = await loginUserUseCase.perform({ email, password });
-    //   SetLoading(false);
-    //   if (result.isLeft()) {
-    //   }
-    // } catch (error) {
-    //   SetLoading(false);
-    // } finally {
-    //   SetLoading(false);
-    // }
+        SetToast(true);
+        return 0;
+      }
+
+      SetErrorOrSuccess({
+        message: 'login user successfully',
+        status: 'Success',
+      });
+      SetToast(true);
+      setTimeout(() => navigate('/'), 2000);
+    } catch (error: any) {
+      SetLoading(false);
+      SetErrorOrSuccess({
+        message: error.message,
+        status: 'Error',
+      });
+    } finally {
+      SetLoading(false);
+    }
   }
 
   return (
-    <FormContainer>
-      <Form>
-        <div>
-          <img src={logo} alt="logo" />
-        </div>
-        <FormContent>
-          <InputForm
-            value={email}
-            SetValue={SetEmail}
-            name="email"
-            label="Email"
-            placeholder="Enter your Email"
-          >
-            <i className="ri-mail-line"></i>
-          </InputForm>
+    <>
+      <FormContainer>
+        <Form>
+          <div>
+            <img src={logo} alt="logo" />
+          </div>
+          <FormContent>
+            <InputForm
+              value={email}
+              SetValue={SetEmail}
+              name="email"
+              label="Email"
+              placeholder="Enter your Email"
+            >
+              <i className="ri-mail-line"></i>
+            </InputForm>
 
-          <InputForm
-            value={password}
-            SetValue={SetPassword}
-            type="password"
-            name="password"
-            label="Password"
-            placeholder="Enter your password"
-          >
-            <i className="ri-lock-password-line"></i>
-          </InputForm>
-          <Button isDisable={false} text="Login" SetActive={handlerSubmit}>
-            <i className="ri-login-circle-line"></i>
-          </Button>
-        </FormContent>
-        <p>
-          You don't have an account? <Link to="/user/signup">SignUp</Link>
-        </p>
-      </Form>
-    </FormContainer>
+            <InputForm
+              value={password}
+              SetValue={SetPassword}
+              type="password"
+              name="password"
+              label="Password"
+              placeholder="Enter your password"
+            >
+              <i className="ri-lock-password-line"></i>
+            </InputForm>
+            <Button text="Login" SetActive={handlerSubmit} spinner={loading}>
+              <i className="ri-login-circle-line"></i>
+            </Button>
+          </FormContent>
+          <p>
+            You don't have an account? <Link to="/user/signup">SignUp</Link>
+          </p>
+        </Form>
+      </FormContainer>
+      <Toast
+        value={toast}
+        SetValue={SetToast}
+        status={errorOrSuccess?.status}
+        message={errorOrSuccess?.message}
+      />
+    </>
   );
 };
