8eb0fae914cfb0d62cc2793d0ab6cdb851fee583
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-08 14:27:34 +0200

    feat: create delete image usecase

---
+++ src/data/usecases/image/DeleteImageUseCase.ts
@@ -0,0 +1,36 @@
+import { left, right } from '@/shared/error-handler/either';
+import { endpoints } from '@/shared/utils';
+import { HttpClient, HttpStatusCode } from '@/data/protocols/http';
+import { UnexpectedError } from '@/domain/errors';
+import { IDeleteImageUseCase } from '@/domain/usecases/image';
+
+export class DeleteImageUseCase implements IDeleteImageUseCase {
+  private readonly httpClient: HttpClient;
+  constructor(httpClient: HttpClient) {
+    this.httpClient = httpClient;
+  }
+
+  async perform(data: IDeleteImageUseCase.Input): IDeleteImageUseCase.Output {
+    try {
+      const request = {
+        ...endpoints.image.delete,
+        param: data.id,
+      };
+      const response = await this.httpClient.request(request);
+
+      switch (response.statusCode) {
+        case HttpStatusCode.badRequest:
+          return left(response.body);
+          break;
+        case HttpStatusCode.ok:
+          return right(response.body);
+          break;
+        default:
+          return left(new UnexpectedError());
+          break;
+      }
+    } catch (error) {
+      return left(new UnexpectedError());
+    }
+  }
+}
--- src/data/usecases/image/index.ts
+++ src/data/usecases/image/index.ts
@@ -1,3 +1,4 @@
 export * from './CreateImageUseCase';
 export * from './GetAllImageUseCase';
 export * from './GetNameImageUseCase';
+export * from './DeleteImageUseCase';
