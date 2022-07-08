b39788e4e002d919de0b4bf5b7335d844821e855
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-08 13:57:34 +0200

    feat: create image usecase

---
+++ src/domain/usecases/image/create-image-usecase.ts
@@ -0,0 +1,12 @@
+import { Image } from '@/domain/entities';
+import { InvalidParamError, UnexpectedError } from '@/domain/errors';
+import { Either } from '@/shared/error-handler/either';
+
+export interface ICreateImageUseCase {
+  perform: (data: ICreateImageUseCase.Input) => ICreateImageUseCase.Output;
+}
+
+export namespace ICreateImageUseCase {
+  export type Input = Image;
+  export type Output = Promise<Either<UnexpectedError | InvalidParamError, { id: string }>>;
+}
