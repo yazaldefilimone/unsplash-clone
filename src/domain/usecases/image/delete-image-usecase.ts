77bbfeb0e30a84154d732f74f4bbfdc3efcb0083
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-08 13:57:44 +0200

    feat: delete image usecase

---
+++ src/domain/usecases/image/delete-image-usecase.ts
@@ -0,0 +1,11 @@
+import { InvalidParamError, UnexpectedError } from '@/domain/errors';
+import { Either } from '@/shared/error-handler/either';
+
+export interface IDeleteImageUseCase {
+  perform: (data: IDeleteImageUseCase.Input) => IDeleteImageUseCase.Output;
+}
+
+export namespace IDeleteImageUseCase {
+  export type Input = { id: string; userId: string };
+  export type Output = Promise<Either<UnexpectedError | InvalidParamError, void>>;
+}
